c3209fcef0fb85ee655517d733e8929eb5a149f6
Anton Istomin <anton@tail-call.ru>
2020-07-12 14:48:36 +0700

    Add JPEG support. Copyright notice.

--- itxt.py
+++ itxt.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
+ #########################################################################
+##/                                                                     \##
+##  Injects text content into a JPEG or PNG input file as close to       ##
+##  beggining of file as possible. Outputs a valid JPEG/PNG file         ##
+##  readable by most software to stdout.                                 ##
+##                                                                       ##
+##  Please note that for JPEG embedded payload shouldn't be larger than  ##
+##  65,536 bytes (64kb). For PNG limit is 4,294,967,296 bytes (~4Gb).    ##
+##                                                                       ##
+##  © 2020 Anton Istomin                                                 ##
+##\                                                                     /##
+ #########################################################################
+
 import struct
 import binascii
 import sys
 
-pngMagic = bytearray(b'\x89PNG\x0d\x0a\x1a\x0a')
+PNG_MAGIC = bytearray(b'\x89PNG\x0d\x0a\x1a\x0a')
+JPG_MAGIC = bytearray(b'\xFF\xD8')
+
+EXIT_NOINPUT = 1
+EXIT_NOTEXT = 2
+EXIT_BADINPUT = 3
 
 def arg(i):
     if i >= len(sys.argv):
@@ -12,18 +30,14 @@ def arg(i):
     return sys.argv[i]
 
 def die(message, code = -1):
-    print('Error: ' + message)
+    print('Error: ' + message, file=sys.stderr)
     exit(code)
 
 def usage():
-    print('Usage: ' + sys.argv[0] + ' <input png> <input txt>')
+    print('Usage: ' + sys.argv[0] + ' <input jpeg/png> <input txt>', file=sys.stderr)
 
 def spew(buf):
     sys.stdout.buffer.write(buf)
-
-def checkMagic(file):
-    leadingBytes = file.read(len(pngMagic))
-    return leadingBytes == pngMagic
 
 def skip_iHDR(file):
     rawLength = file.read(4)
@@ -40,9 +54,16 @@ def spew_iTXt(buf):
     spew(buf)
     spew(struct.pack('>L', crc))
 
+def spew_COM(buf):
+    spew(b'\xFF\xFE')
+    length = len(buf)
+    spew(struct.pack('>H', length))
+    spew(buf)
+
+# Injects iTXt chunk for PNG
 def inject_iTXt(inputFile, textFile):
-    checkMagic(inputFile) or die('input file is not a PNG')
-    spew(pngMagic)
+    inputFile.read(len(PNG_MAGIC))
+    spew(PNG_MAGIC)
 
     skip_iHDR(inputFile)
 
@@ -52,10 +73,31 @@ def inject_iTXt(inputFile, textFile):
     trailer = inputFile.read()
     spew(trailer)
 
-inputFileName = arg(1) or usage() or die('no input file specified')
-textFileName = arg(2) or usage() or die('no itxt content file specified')
+# Inject comment section for JPEG
+def inject_COM(inputFile, textFile):
+    inputFile.read(len(JPG_MAGIC))
+    spew(JPG_MAGIC)
 
-inputFile = open(inputFileName, mode = 'rb')
-textFile = open(textFileName, mode = 'rb')
+    text = textFile.read()
+    spew_COM(text)
 
-inject_iTXt(inputFile, textFile)
+    trailer = inputFile.read()
+    spew(trailer)
+
+def matchesMagic(file, magic):
+    leadingBytes = file.read(len(magic))
+    file.seek(-len(magic), 1)
+    return leadingBytes == magic
+
+inputFileName = arg(1) or usage() or die('no input file specified', EXIT_NOINPUT)
+textFileName = arg(2) or usage() or die('no itxt content file specified', EXIT_NOTEXT)
+
+inputFile = open(inputFileName, mode='rb')
+textFile = open(textFileName, mode='rb')
+
+if matchesMagic(inputFile, PNG_MAGIC):
+    inject_iTXt(inputFile, textFile)
+elif matchesMagic(inputFile, JPG_MAGIC):
+    inject_COM(inputFile, textFile)
+else:
+    die(inputFileName + ': file not supported', EXIT_BADINPUT)
